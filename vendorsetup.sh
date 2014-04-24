769b58325f70a477b597ce83b4dfea7ba1d5d7a4
cr3pt <cr3pt@o2.pl>
2014-04-24 17:34:56 +0200

    fixes

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,5 @@
 # This whole script is a hack. Hoping to replace it soon.
-if [ ! -z "$JENKINS_HOME" ]
-then
-	DEVICE_NAME=${LUNCH#*_} DEVICE_NAME=${DEVICE_NAME%-*}
-	if [ "$DEVICE_NAME" == "p700" ]
+if [ "$TARGET_PRODUCT" == "pa_p700" ]
 	then
 		sh device/lge/p700/patches/apply.sh
-	fi
 fi

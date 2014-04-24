ec267890f7fefdb31e57bbb25dd4377355d83489
cr3pt <cr3pt@o2.pl>
2014-04-24 17:14:41 +0200

    add patches

---
+++ vendorsetup.sh
@@ -0,0 +1,9 @@
+# This whole script is a hack. Hoping to replace it soon.
+if [ ! -z "$JENKINS_HOME" ]
+then
+	DEVICE_NAME=${LUNCH#*_} DEVICE_NAME=${DEVICE_NAME%-*}
+	if [ "$DEVICE_NAME" == "p700" ]
+	then
+		sh device/huawei/p700/patches/apply.sh
+	fi
+fi

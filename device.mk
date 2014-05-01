18190ab3778231ee543a2972a9dc1e952662edd9
cr3pt <cr3pt@o2.pl>
2014-05-01 19:33:19 +0200

    Revert "merge from THLG"
    This reverts commit 56cf9168890db411c5a8e7e65d361c22e9547d3b.

--- device.mk
+++ device.mk
@@ -22,6 +22,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
 
+# Wifi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/wlan.ko:system/lib/modules/wlan.ko
+
 # Misc
 PRODUCT_PACKAGES += \
     libnetcmdiface

d005b95d40e90d6f9345b43974a70a226d1f6911
cr3pt <cr3pt@o2.pl>
2014-08-07 14:17:01 +0200

    next

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,13 +17,8 @@ TARGET_PREBUILT_KERNEL := device/lge/p700/kernel
 #TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.8
 TARGET_KERNEL_SOURCE := kernel/lge/msm7x27a-common
 
-ifeq ($(HAVE_NFC),true)
 TARGET_BOOTLOADER_BOARD_NAME := p700
 TARGET_KERNEL_CONFIG := cyanogenmod_u0_defconfig
-else
-TARGET_BOOTLOADER_BOARD_NAME := p705
-TARGET_KERNEL_CONFIG := cyanogenmod_u0_nonfc_defconfig
-endif
 
 TARGET_RECOVERY_FSTAB := device/lge/p700/recovery.fstab
 
--- device.mk
+++ device.mk
@@ -1,8 +1,6 @@
 $(call inherit-product, device/lge/msm7x27a-common/msm7x27a-common.mk)
 
-ifeq ($(HAVE_NFC),true)
 $(call inherit-product, device/lge/msm7x27a-common/nfc.mk)
-endif
 
 $(call inherit-product-if-exists, vendor/lge/p700/p700-vendor.mk)
 
